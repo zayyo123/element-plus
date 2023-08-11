b621104f26bae586ccc43df7648105479588ee8d
zayyo123 <764439554@qq.com>
2023-08-11 21:25:12 +0800

    读源码注释

--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -9,26 +9,34 @@ export const withInstall = <T, E extends Record<string, any>>(
   main: T,
   extra?: E
 ) => {
+  // 为主类和额外的组件添加install方法
   ;(main as SFCWithInstall<T>).install = (app): void => {
-    for (const comp of [main, ...Object.values(extra ?? {})]) {
+    // 遍历主类和额外的组件，将它们添加到app中
+    for (const comp of [main,...Object.values(extra?? {})]) {
       app.component(comp.name, comp)
     }
   }
 
+  // 如果额外的组件存在，则遍历它们，将它们添加到app中
   if (extra) {
     for (const [key, comp] of Object.entries(extra)) {
       ;(main as any)[key] = comp
     }
   }
+  // 返回主类和额外的组件
   return main as SFCWithInstall<T> & E
 }
 
 export const withInstallFunction = <T>(fn: T, name: string) => {
+  // 为主类和名称添加install方法
   ;(fn as SFCWithInstall<T>).install = (app: App) => {
+    // 将fn的_context设置为app的_context
     ;(fn as SFCInstallWithContext<T>)._context = app._context
+    // 将fn添加到app中
     app.config.globalProperties[name] = fn
   }
 
+  // 返回fn
   return fn as SFCInstallWithContext<T>
 }
 
@@ -36,15 +44,20 @@ export const withInstallDirective = <T extends Directive>(
   directive: T,
   name: string
 ) => {
+  // 为主类和名称添加install方法
   ;(directive as SFCWithInstall<T>).install = (app: App): void => {
+    // 将directive添加到app中
     app.directive(name, directive)
   }
 
+  // 返回directive
   return directive as SFCWithInstall<T>
 }
 
 export const withNoopInstall = <T>(component: T) => {
+  // 为主类和名称添加install方法
   ;(component as SFCWithInstall<T>).install = NOOP
 
+  // 返回component
   return component as SFCWithInstall<T>
 }
--- packages/utils/vue/typescript.ts
+++ packages/utils/vue/typescript.ts
@@ -1,7 +1,16 @@
+// 从 'vue' 模块中导入 AppContext 和 Plugin 类型。
 import type { AppContext, Plugin } from 'vue'
 
+// 定义了一个叫SFCWithInstall的TS类型，它是由两个类型 T 和 Plugin 进行交叉混和
 export type SFCWithInstall<T> = T & Plugin
 
 export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
   _context: AppContext | null
 }
+
+// 定义了一个泛型类型 `SFCInstallWithContext<T>`，它是由 `SFCWithInstall<T>` 类型和 `{ _context: AppContext | null }` 对象类型合并而成的。
+
+// - `SFCWithInstall<T>` 是一个带有安装函数的单文件组件（Single File Component）。
+// - `_context` 是一个可选属性，它的类型为 `AppContext | null`。该属性用来存储应用程序上下文信息。
+
+// 通过将这两个类型进行合并，我们创建了一个新的泛型类型 `SFCInstallWithContext<T>`。使用此类型时，我们既可以获得带有安装函数的单文件组件特性，又可以访问 `_context` 属性来获取或设置应用程序上下文信息。
