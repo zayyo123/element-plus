9d22956c12a4b68d068ea3e04c62f6b7967cfb39
zayyo123 <764439554@qq.com>
2023-08-11 22:15:43 +0800

    读源码注释

--- packages/components/affix/src/affix.ts
+++ packages/components/affix/src/affix.ts
@@ -9,10 +9,12 @@ import type { ExtractPropTypes } from 'vue'
 import type { ZIndexProperty } from 'csstype'
 import type Affix from './affix.vue'
 
+// 定义affix组件的props
 export const affixProps = buildProps({
   /**
    * @description affix element zIndex value
    * */
+  // affix元素的zIndex属性
   zIndex: {
     type: definePropType<ZIndexProperty>([Number, String]),
     default: 100,
@@ -20,6 +22,7 @@ export const affixProps = buildProps({
   /**
    * @description target container. (CSS selector)
    */
+  // target container. (CSS selector)
   target: {
     type: String,
     default: '',
@@ -27,6 +30,7 @@ export const affixProps = buildProps({
   /**
    * @description offset distance
    * */
+  // offset distance
   offset: {
     type: Number,
     default: 0,
@@ -34,19 +38,26 @@ export const affixProps = buildProps({
   /**
    * @description position of affix
    * */
+  // position of affix
   position: {
     type: String,
     values: ['top', 'bottom'],
     default: 'top',
   },
 } as const)
+// 定义affix组件的props类型
 export type AffixProps = ExtractPropTypes<typeof affixProps>
 
+// 定义affix组件的emits
 export const affixEmits = {
+  // scroll事件
   scroll: ({ scrollTop, fixed }: { scrollTop: number; fixed: boolean }) =>
     isNumber(scrollTop) && isBoolean(fixed),
+  // change事件
   [CHANGE_EVENT]: (fixed: boolean) => isBoolean(fixed),
 }
+// 定义affix组件的emits类型
 export type AffixEmits = typeof affixEmits
 
+// 定义affix组件的实例
 export type AffixInstance = InstanceType<typeof Affix>
